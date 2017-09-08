c3658d9bf8153886c371cfe4cdcb6108b6ca59d2
Jack Bittiner <jackbittiner@hotmail.com>
2017-09-08 21:30:39 +0100

    Input Validator returns true if input is an integer above 1

--- spec/inputValidatorSpec.js
+++ spec/inputValidatorSpec.js
@@ -19,5 +19,10 @@ describe('InputValidator', function() {
     it('throws an error if the input is less than 1', function() {
       expect(function(){ inputValidator.validateInput(32.3) } ).toThrow(new Error("Please input a whole number above 1."))
     });
+
+    it('returns true if the input is an integer above 1', function() {
+      expect(inputValidator.validateInput('20')).toEqual(true)
+      expect(inputValidator.validateInput(20)).toEqual(true)
+    });
   });
 });
--- src/inputValidator.js
+++ src/inputValidator.js
@@ -5,6 +5,8 @@ InputValidator.prototype.validateInput = function(input) {
   var userInput = parseInt(input)
     if (isNaN(userInput) || userInput < 1 || input % 1 !== 0) {
       throw new Error ('Please input a whole number above 1.');
+    } else {
+      return true
     }
 }
 
