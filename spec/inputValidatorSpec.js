4c254fa9d46370e5b7d597d77090b49e579322c1
Jack Bittiner <jackbittiner@hotmail.com>
2017-09-08 18:11:24 +0100

    Added test for input having to be >1

--- spec/inputValidatorSpec.js
+++ spec/inputValidatorSpec.js
@@ -11,5 +11,9 @@ describe('InputValidator', function() {
     it('throws an error if the input is not a number', function() {
       expect(function(){ inputValidator.validateInput('a') } ).toThrow(new Error("Please input a whole number above 1."))
     });
+
+    it('throws an error if the input is less than 1', function() {
+      expect(function(){ inputValidator.validateInput(-10) } ).toThrow(new Error("Please input a whole number above 1."))
+    });
   });
 });
