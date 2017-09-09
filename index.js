62bc345b69b31496df9332829b634c866a2206c2
Jack Bittiner <jackbittiner@hotmail.com>
2017-09-09 13:12:02 +0100

    User input is validated on request

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var readline = require('readline');
 var mT = require('./src/multiplicationTable')
 var pG = require('./src/primeGenerator')
 var tG = require('./src/tableGenerator')
+var iV = require('./src/inputValidator')
 
 
 const rl = readline.createInterface({
@@ -9,14 +10,25 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('How many primes would you like to multiply? ', (userInput) => {
-  rl.close();
-  var numberOfPrimes = parseInt(userInput);
-  var multiplicationTable = new mT.MultiplicationTable();
-  var primeGenerator = new pG.PrimeGenerator();
-  var tableGenerator = new tG.TableGenerator();
-  var primes = primeGenerator.getPrimes(numberOfPrimes);
-  var tableData = multiplicationTable.formMultiplicationTable(primes);
-  var finalResults = tableGenerator.generateTable(tableData);
-  console.log(finalResults)
-});
+
+var processUserInput = function() {
+  rl.question('How many primes would you like to multiply? ', (userInput) => {
+    var inputValidator = new iV.InputValidator()
+    if(inputValidator.validateInput(userInput)) {
+      rl.close();
+      var numberOfPrimes = parseInt(userInput);
+      var multiplicationTable = new mT.MultiplicationTable();
+      var primeGenerator = new pG.PrimeGenerator();
+      var tableGenerator = new tG.TableGenerator();
+      var primes = primeGenerator.getPrimes(numberOfPrimes);
+      var tableData = multiplicationTable.formMultiplicationTable(primes);
+      var finalResults = tableGenerator.generateTable(tableData);
+      console.log(finalResults)
+    } else {
+      console.log('Please input a whole number that is at least 1')
+      processUserInput();
+    }
+  });
+}
+
+processUserInput();
